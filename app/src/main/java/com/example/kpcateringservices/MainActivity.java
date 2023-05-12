b54df661b24a396ecb616a8059d68a65795dcd4e
Kaustubh7276 <nikalekaustubh7276@gmail.com>
2023-05-12 13:25:52 +0530

    initial

---
+++ app/src/main/java/com/example/kpcateringservices/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.kpcateringservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        LoginFragment login=new LoginFragment();
+
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.LoginRegister,login);
+
+        fragmentTransaction.commit();
+    }
+
+    public void changeFragment(String id)
+    {
+        if(id=="Register") {
+            RegisterFragment register = new RegisterFragment();
+            FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction1.replace(R.id.LoginRegister, register);
+            fragmentTransaction1.commit();
+        }
+        else if(id=="Login"){
+            LoginFragment login =new LoginFragment();
+            FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction1.replace(R.id.LoginRegister, login);
+            fragmentTransaction1.commit();
+        }
+
+    }
+}
